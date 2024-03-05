9caeed37b5702432a90d5d8bfb21833f912cd9ed
Wilson Almonte <almontereyesw@gmail.com>
2024-03-05 00:52:38 -0400

    Optimizacion del codigo

--- src/hooks/useLocalStorage.tsx
+++ src/hooks/useLocalStorage.tsx
@@ -1,30 +1,3 @@
-// import { useState, useEffect } from "react"
-
-
-// export default function useLocalstorage(key: string){
-//     const localStorage = window.localStorage.getItem(key)
-//     const [dataLocal, setDataLocal] = useState(JSON.parse(localStorage ?? '{}'));
-
-
-
-//     const setItem = (value: unknown)=>{
-//         console.log(value)
-//         const saveData = window.localStorage.setItem(key, JSON.stringify([{data: value}]))
-//         if(!dataLocal) return saveData;
-//         const u = [{...dataLocal}, value];
-//         console.log(u)
-//         setDataLocal([...dataLocal, value])
-//         window.localStorage.setItem(key, JSON.stringify(dataLocal))
-
-//         console.log(dataLocal)
-//     }
-
-
-
-//     return {setItem, dataLocal}
-// }
-
-
 import React, { useState, useEffect } from "react";
 
 
