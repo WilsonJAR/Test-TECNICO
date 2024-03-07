011ab59e7b03582416301374c5947333fb08105d
Wilson Almonte <almontereyesw@gmail.com>
2024-03-07 16:56:45 -0400

    Update form for new package

--- src/Form/form.tsx
+++ src/Form/form.tsx
@@ -1,19 +1,47 @@
 import { useForm } from "react-hook-form"
+import { useEffect } from "react"
 import { InputText } from 'primereact/inputtext'
 import {Button} from 'primereact/button'
 import useLocalstorage from "../hooks/useLocalStorage"
 import { Formulario, Title, HeaderHome } from "../styles"
-
+import {
+    FormControl,
+    FormGroup,
+    useFormConfig,
+    WControlData,
+    WForm,
+    WFormControl,
+  } from "rectangular-forms";
+  
 const Form = () => {
-
-    const {register, handleSubmit, formState:{errors}} = useForm({defaultValues:{
-        user: "",
-        lastName: "",
-        phoneNumber: "",
-        birthdate: ""
-
-    }});
     const {agregarObjeto} = useLocalstorage("usuario")
+
+    const formConfig = useFormConfig({
+        createForm: (data) => {
+          const form = new FormGroup({
+            user: new FormControl(),
+            lastname: new FormControl(),
+            phoneNumber: new FormControl(), 
+            birthdate: new FormControl()
+
+          });
+          form.patchValue(data);
+          return form;
+        },
+        onSubmit: (form) => {
+            agregarObjeto(form.value);
+          },
+      });
+      const { loadSucceed } = formConfig;
+
+      useEffect(() => {
+        loadSucceed({
+          user: "",
+          lastname: "",
+          phonenumber: "",
+          birthday: ""
+        });
+      }, [loadSucceed]);
 
     return ( 
        <div>
@@ -21,41 +49,82 @@ const Form = () => {
             <HeaderHome >
                 <Title style={{margin: "0 auto"}}>Formulario de registro</Title>
             </HeaderHome>
-      
-            <Formulario 
-            onSubmit={handleSubmit((data)=>{
-                agregarObjeto(data)
-            })}
-            >
 
-                <InputText 
-                className="p-inputtext-lg"
-                {...register ("user", {required: "required"}) }
-                placeholder="Nombre"
-                />
-                <p>{errors.user?.message}</p>
 
-                <InputText 
-                className="p-inputtext-lg"
-                {...register ("lastName", {required: "required"}) }
-                placeholder="Apellido"
-                />
-                <InputText 
-                className="p-inputtext-lg"
-                {...register ("phoneNumber", {required: "required"}) }
-                placeholder="Telefono"
-                />
 
-                <InputText 
-                className="p-inputtext-lg"
-                type="date"
-                {...register ("birthdate", {required: "required"}) }
-                placeholder="Fecha_nac"
-                />
+            <WForm formConfig={formConfig}>
+                <WControlData />
+                <WFormControl 
+                name="user"
+                errorMessages={{
+                    required: "El nombre es requerido.",
+                  }}
+                  >
+                    {({ control }) => {
+                    const { value, onChange, onBlur, disabled } = control;
+                    return (
+                        <InputText 
+                        className="p-inputtext-lg"
+                        value={value || ""}
+                        onChange={onChange}
+                        onBlur={onBlur}
+                        disabled={disabled}
+                        placeholder="Nombre"
+                        />
+                    );
+                    }}
 
-                <Button label="Submit" icon="pi pi-check" iconPos="right" />
                 
-            </Formulario>
+                </WFormControl>
+                <WFormControl name="lastname">
+                    {({ control }) => {
+                    const { value, onChange, onBlur, disabled } = control;
+                    return (
+                        <InputText 
+                        className="p-inputtext-lg"
+                        value={value || ""}
+                        onChange={onChange}
+                        onBlur={onBlur}
+                        disabled={disabled}
+                        placeholder="Apellido"
+                        />
+                    );
+                    }}
+                </WFormControl>
+                <WFormControl name="phoneNumber">
+                    {({ control }) => {
+                    const { value, onChange, onBlur, disabled } = control;
+                    return (
+                        <InputText 
+                        className="p-inputtext-lg"
+                        type="tel"
+                        value={value || ""}
+                        onChange={onChange}
+                        onBlur={onBlur}
+                        disabled={disabled}
+                        placeholder="Telefono"
+                        />
+                    );
+                    }}
+                </WFormControl>
+                <WFormControl name="birthdate">
+                    {({ control }) => {
+                    const { value, onChange, onBlur, disabled } = control;
+                    return (
+                        <InputText 
+                        className="p-inputtext-lg"
+                        type="date"
+                        value={value || ""}
+                        onChange={onChange}
+                        onBlur={onBlur}
+                        disabled={disabled}
+                        placeholder="Fecha Nac"
+                        />
+                    );
+                    }}
+                </WFormControl>
+                <Button label="Submit" icon="pi pi-check" iconPos="right" />
+            </WForm>
         </header>
        </div>
      );
