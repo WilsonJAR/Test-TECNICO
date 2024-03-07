22bfcb611f17dd596bfce386e8a7c72249609c2f
Wilson Almonte <almontereyesw@gmail.com>
2024-03-07 17:30:32 -0400

    Added consume api with rxjs

--- src/Home/home.tsx
+++ src/Home/home.tsx
@@ -1,28 +1,30 @@
 
 import { useState, useEffect } from "react";
-import allCharacter from "../data/cardData";
 import { Card } from "primereact/card";
 import { Title, HeaderHome, CardDisplay, InfoText } from "../styles";
-
+import { ajax } from "rxjs/ajax";
+import { pluck } from "rxjs";
 
         
 const Home = () => {
     const [characters, setCharacters] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = await allCharacter();
-        setCharacters(data.results); 
-      } catch (error) {
-        console.error("Error fetching data:", error);
+    const getData = () => {
+      return ajax
+        .get("https://rickandmortyapi.com/api/character")
+        .pipe(
+          pluck("response"),
+        )
+        .subscribe((res: any) => {
+          setCharacters(res.results);
+        });
       }
-    };
+      
+      
+      useEffect(()=> {
+        getData();
+      }, []);
 
-    fetchData();
-  }, []);
-
-  console.log(characters)
 
     return ( 
         <div className="content">
@@ -33,7 +35,7 @@ const Home = () => {
                 
                 <section>
                     <CardDisplay>
-                        {characters.map((data, key)=>(
+                        {characters.map((data: any, key)=>(
                         <Card title={data.name} subTitle={data.status} header={()=>(<img src={data.image}/>)} className="md:w-25rem">
                           <InfoText className="desripition">Species: {data.species}</InfoText>
                           <InfoText className="type">Type: {data.type}</InfoText>
